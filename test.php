8d0b7bb41693f0d2a6376017f5713e5084ce425f
vchiarello <vito.chiarello@gmail.com>
2017-09-21 14:42:12 +0200

    a

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
-<h1><?php echo "Ciao Vito"; ?></h1>
+<h1><?php echo "Ciao Vito1"; ?></h1>
 <?php $statement = "PHP Open and Close Tags";?>
 <?php $timezoneIderntifiers = DateTimeZone::listIdentifiers(); ?>
 <?=$statement;?>
